b7d2fd6f1512644fea2e41d1e94cd6837bad79d8
Igor Belyaev <IYBelyaev@alfabank.ru>
2020-08-11 18:59:21 +0300

    fix(*): test

--- .github/workflows/scripts/create-artifact.sh
+++ .github/workflows/scripts/create-artifact.sh
@@ -3,11 +3,4 @@
 set -x
 set -e
 
-
-echo 'Create new version onl y for master push '
-
-# yarn --frozen-lockfile
-
-# yarn --cwd ${{ github.workspace }} semantic-release
-
-ls -a
+yarn --frozen-lockfile && yarn semantic-release
