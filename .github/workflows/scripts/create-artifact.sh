4ee8007b6a0d40dbc57b396964ef718a3169b53d
Igor Belyaev <IYBelyaev@alfabank.ru>
2020-08-12 15:01:31 +0300

    fix(*): add

--- .github/workflows/scripts/create-artifact.sh
+++ .github/workflows/scripts/create-artifact.sh
@@ -3,4 +3,17 @@
 set -x
 set -e
 
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc && npm whoami
+#!/bin/sh
+
+set -x
+set -e
+
+echo 'I am here'
+
+echo $NPM_TOKEN
+
+echo '!!!!!!!!!!!!!!!!!!!!!!!'
+
+echo $secrets_NPM_TOKEN
+
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc && npm whoami
